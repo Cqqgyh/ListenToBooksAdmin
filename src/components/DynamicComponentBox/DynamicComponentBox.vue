aa1047e366b8c495ee20abdbd790d43f70413717
cqq <945327638@qq.com>
2023-05-26 18:31:00 +0800

    feat: 增加动态路由（tsx中使用）

---
+++ src/components/DynamicComponentBox/DynamicComponentBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <component :is="is" v-bind="$attrs"></component>
+</template>
+<script setup lang="ts">
+defineProps<{
+  is: string
+}>()
+</script>
+<style scoped lang="scss"></style>
